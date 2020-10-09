d1d438df0f1603e66814abfa1175f73b99ae46a2
Sean O'Donnell <sean@Seans-iMac.local>
2020-10-08 22:20:33 -0400

    Added router, reducer, and state provider. Created login page and login css.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Style/App.css';
 import Sidebar from './Components/Sidebar';
-import Chat from './Components/Chat'
+import Chat from './Components/Chat';
+import Login from './Components/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useStateValue } from './StateProvider'
 
-function App() {
+function App(props) {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
-      <h1>CheezApp </h1>
 
+      {!user ? (
+        <Login />
+      ) : (
       <div className="app__body">
-        <Sidebar />
-        <Chat />
+        <Router>
+            <Sidebar />
+
+            <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Chat />
+            </Route>
+            
+          </Switch>
+        </Router>
       </div>
+      )}
     </div>
   );
+  
 }
 
 export default App;
--- src/Components/SidebarChat.js
+++ src/Components/SidebarChat.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import '../Style/SidebarChat.css';
 import { Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import db from '../firebase';
 
 function SidebarChat(props) {
     const [seed, setSeed] = useState('');
@@ -10,6 +12,9 @@ function SidebarChat(props) {
 
         if (roomName){
             // Add API work
+            db.collection('rooms').add({
+                name: roomName,
+            });
         }
     };
 
@@ -19,13 +24,16 @@ function SidebarChat(props) {
     }, []);
 
     return !props.addNewChat ? (
-        <div className="sidebarChat">
+        <Link to={`/rooms/${props.id}`}>
+            <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
                 <h2>{props.name}</h2>
                 <p>last message...</p>
             </div>
         </div>
+        </Link>
+        
     ) : (
         <div onClick={createChat}
         className="sidebarChat">
